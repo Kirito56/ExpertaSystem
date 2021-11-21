760e053401bdd85fbad738a024698cfb1aaf00a2
SkillFi <bimbom2345@gmail.com>
2021-11-21 17:22:38 +0200

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import random
-
-def auth(i):
-    for j in range(i):
-        print(f'{random.randint(0,9)}{random.randint(0,9)}{random.randint(0,9)}{random.randint(0,9)}{random.randint(0,9)}{random.randint(0,9)}')
-
-auth(6)
